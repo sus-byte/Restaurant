19500cdea1c7c19bf3e49e6236cc66752519efd3
Susan <susanjati11@gmail.com>
2024-09-23 11:04:47 +0545

    third commit

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { images, data } from "../../constants";
+import { images } from "../../constants";
 import { SubHeading } from "../../components";
 
 const FindUs = () => (
